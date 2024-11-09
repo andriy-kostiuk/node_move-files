51a1273944847b0ebd45162fd36f6f48c6405bc2
Andrii <andriykostiuk1@gmail.com>
2024-11-09 16:27:53 +0200

    add solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,62 @@
-// write code here
+/* eslint-disable no-console */
+const fs = require('fs');
+const path = require('path');
+
+const [sourcePath, destinationPath] = process.argv.slice(2);
+
+const moveFile = () => {
+  if (!sourcePath || !destinationPath) {
+    console.error('No source or destination provided');
+
+    return;
+  }
+
+  if (!fs.existsSync(sourcePath)) {
+    console.error("Source file doesn't exist");
+
+    return;
+  }
+
+  let destinationPathFull = destinationPath;
+  let isDestinationDirectory = false;
+
+  try {
+    isDestinationDirectory = fs.lstatSync(destinationPath).isDirectory();
+  } catch (err) {
+    if (err.code !== 'ENOENT') {
+      console.error('Error reading destination path:', err);
+
+      return;
+    }
+  }
+
+  if (isDestinationDirectory) {
+    destinationPathFull = path.join(destinationPath, path.basename(sourcePath));
+  }
+
+  if (path.resolve(sourcePath) === path.resolve(destinationPathFull)) {
+    console.log('Source and destination paths are the same. Nothing to move.');
+
+    return;
+  }
+
+  fs.copyFile(sourcePath, destinationPathFull, (err) => {
+    if (err) {
+      console.error(err);
+
+      return;
+    }
+
+    fs.rm(sourcePath, (error) => {
+      if (error) {
+        console.error(error);
+
+        return;
+      }
+
+      console.log('The file was moved successfully.');
+    });
+  });
+};
+
+moveFile();
